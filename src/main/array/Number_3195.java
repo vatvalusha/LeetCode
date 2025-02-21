aef5ac9c924c8471e930a89738a2b471325f2965
Valerii <vatvalusha@gmail.com>
2025-02-20 22:03:14 -0500

    Add method to calculate minimum area enclosing 1s
    Introduce `minimumArea` method to determine the smallest rectangular area that encloses all cells with value 1 in the grid. The method calculates the top, bottom, left, and right boundaries iteratively and then computes the area.

--- src/main/array/Number_3195.java
+++ src/main/array/Number_3195.java
@@ -1,7 +1,70 @@
 package array;
 
 public class Number_3195 {
+
+
     public int minimumArea(int[][] grid) {
+
+        int top = -1, bottom = -1, left = -1, right = -1;
+        int rowSize = grid.length, columnSize = grid[0].length;
+
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < columnSize; j++) {
+                if (grid[i][j] == 1) {
+                    top = i;
+                    break;
+                }
+            }
+
+            if (top != -1) {
+                break;
+            }
+        }
+
+        for (int i = 0; i < columnSize; i++) {
+            for (int j = 0; j < rowSize; j++) {
+                if (grid[j][i] == 1) {
+                    left = i;
+                    break;
+                }
+            }
+
+            if (left != -1) {
+                break;
+            }
+        }
+
+        for (int i = rowSize - 1; i >= 0; i--) {
+            for (int j = 0; j < columnSize; j++) {
+                if (grid[i][j] == 1) {
+                    bottom = i;
+                    break;
+                }
+            }
+
+            if (bottom != -1) {
+                break;
+            }
+        }
+
+        for (int i = columnSize - 1; i >= 0; i--) {
+            for (int j = 0; j < rowSize; j++) {
+                if (grid[j][i] == 1) {
+                    right = i;
+                    break;
+                }
+            }
+
+            if (right != -1) {
+                break;
+            }
+        }
+
+        return (right - left + 1) * (bottom - top + 1);
+
+    }
+
+    public int minimumAreaMain(int[][] grid) {
 
         int minR = Integer.MAX_VALUE;
         int maxR = Integer.MIN_VALUE;

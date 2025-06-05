6d209e5ddb52b4c7bdf9b65f632d695b2a7c2480
Valerii <vatvalusha@gmail.com>
2025-06-05 09:23:58 -0400

    Add MajorityElement_169 implementation and unit tests
    Implement the `majorityElement` function using a sorting and counting approach to find the majority element in an array. Add parameterized unit tests to validate the solution against various test cases.

---
+++ src/main/array/MajorityElement_169.java
@@ -0,0 +1,34 @@
+package array;
+
+class MajorityElement_169 {
+
+    public int majorityElement(int[] nums) {
+        Sorting.bubbleSort(nums);
+        int element = nums[0];
+        int stableCounter = nums[0];
+        int p1 = 0;
+        int counter = 0;
+        for (int p2 = 0; p2 < nums.length; p2++) {
+            if (nums[p1] == nums[p2]) {
+                counter++;
+                if(counter > stableCounter){
+                    stableCounter = counter;
+                    element = nums[p1];
+                }
+            }
+
+            if (nums[p1] != nums[p2]) {
+                if(counter > stableCounter){
+                    stableCounter = counter;
+                    element = nums[p1];
+                }
+                int temp = p1;
+                p1 = p2;
+                p2 = temp;
+                counter = 1;
+            }
+        }
+
+        return element;
+    }
+}
